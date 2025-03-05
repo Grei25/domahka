b96ba39633512744e6faf84e54e949f80b84070c
Grei25 <sepfiroti@gmail.com>
2025-03-05 22:29:09 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 0;
+        if (a>0) {
+            System.out.println("положительное " + a);
+        } else if (a<0){
+            System.out.println("отрицательное " + a );
+
+
+    }else{
+        System.out.println("нейтральный " + a );
+    }
+        int b = 10;
+        boolean  =
+        if (b/2){
+
+        }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+    }
+
