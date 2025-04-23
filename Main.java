81d6f6a7b005bb9021932db7960ab117ac69877a
Grei25 <sepfiroti@gmail.com>
2025-04-24 00:39:16 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,53 +1,23 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    /// Пример с урока
-//    public static char[] myTrim(char[] str){
-//        int countL = 0 , countR = 0;
-//        for (int i = 0; i < str.length; i++) {
-//            if (str[i] == ' ' ){
-//                countL++;
-//            }else{
-//                break;
-//            }
-//        }
-//
-//        for (int i = str.length-1; i>= 0; i--) {
-//            if (str[i] == ' ' ){
-//                countR++;
-//            }else{
-//                break;
-//            }
-//        }
-//
-//        //                           11         3          3   =>   5
-//        char[] result = new char[str.length - countL - countR];
-//        //                                8
-//        for (int i = countL,j = 0; i < str.length - countR; i++,j++) {
-//            result[j] = str[i];
-//        }
-//
-//        return result;
-//    }
+    public static void printArr(int[] arr, int size) {
+        if (size < 1 || arr == null || arr.length == 0) {
+            return;
+        }
+        System.out.print(arr[arr.length - size] + " ");
+        printArr(arr, size - 1);
+    }
+
+
+
 
     public static void main(String[] args) {
-        String str =("Albert Einstein");
-        char[] str1 =str.toCharArray();;
+        int [] num ={1,2,3,4,5};
+        printArr(num,num.length);
 
-//        -     equals("Farid"));
-//        -     endsWith("dlayev"));
-//        -     startsWith("Abdullayev"));
-//        -     replace('F','Z'));
 
-//        -     toCharArray());
-//        -     equalsIgnoreCase("Farid"));
-//        -     substring(10));
-//        -     substring(10,20));
-//        +     trim());
 
-//        -     contains("  "));
-//        -     split("Farid"));
-//        -     replace("Farid","Dima"));
 
 
 
@@ -70,38 +40,6 @@ public class Main {
 
 
 
-//       1+  str.length()
-        //1
-//        System.out.println(str.length());
-//        System.out.println(Mylength(str1));
-//       2+  str.indexOf('a',5)
-        //2
-//        System.out.println(str.indexOf('a',1));
-//        int otvet=myindexOf(str1,'e',0);
-//        System.out.println(otvet);
-//       3+  str.lastIndexOf('a')
-        //3
-//        System.out.println(str.lastIndexOf('a'));
-//        System.out.println(mylastIndexOf(str1,'e'));
-//       4+  str.lastIndexOf('a',5)
-        //4
-//        System.out.println(str.lastIndexOf('e',1));
-//        System.out.println(mylastIndexOf(str1,'e',1));
-//       5+  str.toLowerCase()
-        //5
-//        System.out.println(str.toLowerCase());
-//        System.out.println(myToUpperCase(str1));
-//       6+  charAt(10)
-        //6
-//        System.out.println(str.charAt(2));
-//        System.out.println(mycharAt(str1,2));
-//       7+  isEmpty()
-        //7
-//        System.out.println(str.isEmpty());
-//       System.out.println(myisEmpty(str1));
-        //8
-//        System.out.println(str.isBlank());
-//        System.out.println(myisBlank(str1));
 
 
 
@@ -110,57 +48,5 @@ public class Main {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
